175832f6ff59eec1bc6df82b1a572e448ad19d95
George Hickman <george@ghickman.co.uk>
2012-04-08 00:04:32 +0100

    Tidy cache view

--- cache/views.py
+++ cache/views.py
@@ -9,12 +9,12 @@ class CacheView(DetailView):
         cached_item = cache.get('item')
         if cached_item:
             return cached_item
+
+        try:
+            item = Item.objects.get(pk=1)
+        except Item.DoesNotExist:
+            item = Item.objects.create(name='Herp Derp', body='Herp Derpin all over the USA')
         else:
-            try:
-                item = Item.objects.get(pk=1)
-            except Item.DoesNotExist:
-                item = Item.objects.create(name='Herp Derp', body='Herp Derpin all over the USA')
-            else:
-                cache.set('item', item, 300)
-                return item
+            cache.set('item', item, 300)
+            return item
 
