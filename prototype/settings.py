cf21fc70bb90e1e4d2bbd2c4d39ca734eb513a84
George Hickman <george@ghickman.co.uk>
2012-04-07 15:18:42 +0100

    Use sqlite by default - heroku will take care of the db in production

--- prototype/settings.py
+++ prototype/settings.py
@@ -16,12 +16,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'prototype',                      # Or path to database file if using sqlite3.
-        'USER': 'prototype',                      # Not used with sqlite3.
-        'PASSWORD': 'prototype',                  # Not used with sqlite3.
-        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite',
     }
 }
 
